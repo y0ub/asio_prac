aa630ac2589079cce2f8dc97db5c9b1bbb0cbf6a
y0-e <y0lit7515@gmail.com>
2022-11-07 17:57:06 +0900

    fix directory.

---
+++ timer/building_arguments_to_a_completion_handler/src/main.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <boost/asio.hpp>
+#include <boost/bind/bind.hpp>
+
+void print(
+    const boost::system::error_code &,
+    boost::asio::steady_timer *t,
+    int *count)
+{
+  if (*count >= 5)
+    return;
+
+  std::cout << "count is " << *count << std::endl;
+  ++(*count);
+
+  // move the expiry time for the timer along
+  // by one second from the previous expiry time.
+  t -> expires_at(t -> expiry() + boost::asio::chrono::seconds(1));
+
+  t -> async_wait(boost::bind(
+        print,
+        boost::asio::placeholders::error,
+        t,
+        count));
+}
+
+int main()
+{
+  boost::asio::io_context io;
+
+  int count = 0;
+
+  boost::asio::steady_timer t(io, boost::asio::chrono::seconds(1));
+  t.async_wait(boost::bind(
+        print,
+        boost::asio::placeholders::error,
+        &t,
+        &count));
+
+  io.run();
+
+  std::cout << "Final count is " << count << std::endl;
+
+  return 0;
+}
